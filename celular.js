0adf9c7b695d2e396eceb915539bd36c99b9bd91
Frank Jiménez <frank.jimbar.79@gmail.com>
2026-02-04 19:29:43 +0100

    Acabado el ejercicio del celular

--- celular.js
+++ celular.js
@@ -21,8 +21,61 @@ class Celular {
     this.#contactos = [];
     this.#llamadas = [];
   }
-}
 
+  agregarContacto(contacto) {
+    const existe = this.#contactos.find((elemento) => {
+      return (
+        elemento.nombre === contacto.nombre ||
+        elemento.numero === contacto.numero
+      );
+    });
+    if (!existe) {
+      this.#contactos.push(contacto);
+    } else {
+      return false;
+    }
+  }
+
+  buscarPorNombre(nombre) {
+    const existeNombre = this.#contactos.find(
+      (elemento) => elemento.nombre === nombre,
+    );
+    return existeNombre;
+  }
+
+  buscarPorNumero(numero) {
+    return this.#contactos.find((elemento) => elemento.numero === numero);
+  }
+
+  llamar(nombre) {
+    const contacto = this.#contactos.find(
+      (elemento) => elemento.nombre === nombre,
+    );
+    if (!contacto) return false;
+    this.#llamadas.push(
+      `Llamada a ${contacto.nombre} con numero ${contacto.numero} realizaada`,
+    );
+    return [...this.#llamadas];
+  }
+
+  verContactos() {
+    return this.#contactos
+      .map((contacto) => `${contacto.nombre}: ${contacto.numero} `)
+      .join("\n");
+  }
+
+  verHistorial() {
+    return this.#llamadas.map((llamada) => `${llamada}`).join("\n");
+  }
+}
 const c = new Celular();
 
 console.log(c);
+c.agregarContacto({ nombre: "Juan", numero: "123456789" });
+c.agregarContacto({ nombre: "Frank", numero: "234567328" });
+console.log(c);
+console.log(c.buscarPorNombre("Juan"));
+console.log(c.buscarPorNumero("123456789"));
+console.log(c.llamar("Frank"));
+console.log(c.verContactos());
+console.log(c.verHistorial());
